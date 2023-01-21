b6175afcad6d447e655d3caa84859e6ccf294951
FaiKoprenca <122107842+FaiKop@users.noreply.github.com>
2023-01-21 18:44:43 +0100

    Implementation of Client Handler

---
+++ src/Client/ClientHandler.java
@@ -0,0 +1,46 @@
+package Client;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable{
+
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUsername;
+
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUsername = bufferedReader.readLine();
+            clientHandlers.add(this);
+            //TODO
+            //broadcastMessage("Server: " + clientUsername + " has entered the chat!");
+        } catch (IOException ex){
+            //todo
+            //closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    @Override
+    public void run() {
+        String messageFromClient;
+
+        while (socket.isConnected()){
+            try {
+                messageFromClient = bufferedReader.readLine();
+                //broadcastMessage(messageFromClient);
+            } catch (IOException ex){
+                //closeEverything(socket, bufferedReader, bufferedWriter);
+            }
+        }
+
+    }
+}
+
